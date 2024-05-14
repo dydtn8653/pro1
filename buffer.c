97351a9fd023b7d014f898b61f414a05d4ffcc6c
dydtn8653 <55014664+dydtn8653@users.noreply.github.com>
2024-05-14 13:07:56 +0900

    Update buffer.c
    /*런타임이 작을때는 어쩔때는 좋게 결과가 나오는데 
    런타임을 1000000으로 했을때는 오류가 무조건 발생함
    미소비 되었다고 버퍼문제인가? 로그배열 문제인가?
    */

--- buffer.c
+++ buffer.c
@@ -1,126 +1,117 @@
+
+/*런타임이 작을때는 어쩔때는 좋게 결과가 나오는데 
+런타임을 1000000으로 했을때는 오류가 무조건 발생함
+미소비 되었다고 버퍼문제인가? 로그배열 문제인가?
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdatomic.h>
-/*런타임이 작을때는 어쩔때는 좋게 결과가 나오는데 
-런타임을 1000000으로 했을때는 오류가 무조건 발생함
-미소비 되었다고 뜸 그리고 출력했을때 생산자가 2개씩 뜸
-*/
-#define N 8
-#define MAX 10240
-#define BUFSIZE 4
-#define RUNTIME 100000    /* 출력량을 제한하기 위한 실행시간 (마이크로초) */
-#define RED "\e[0;31m"
-#define RESET "\e[0m"
 
-int buffer[BUFSIZE];
-_Atomic int in = 0;
-_Atomic int out = 0;
-_Atomic int counter = 0;
-_Atomic int next_item = 0;
+#define N 8                         // 스레드 수
+#define MAX 10240                   // 작업 로그 배열 크기
+#define BUFSIZE 4                   // 버퍼 크기
+#define RUNTIME 1000                // 실행 시간 제한 (마이크로초)
+#define RED "\e[0;31m"              // 빨간색 출력용 ANSI 코드
+#define RESET "\e[0m"               // ANSI 코드 리셋
 
-int task_log[MAX][2]; // 생산된 아이템과 소비된 아이템의 로그를 기록하는 배열
-int produced = 0; // 생산된 아이템의 수
-int consumed = 0; // 소비된 아이템의 수
+int buffer[BUFSIZE];                // 버퍼
+int in = 0;                          // 생산자가 아이템을 넣을 위치
+int out = 0;                         // 소비자가 아이템을 꺼낼 위치
+_Atomic int counter = 0;             // 버퍼 안의 현재 아이템 수
+_Atomic int next_item = 0;           // 다음에 생산될 아이템 번호
 
-bool alive = true; // 스레드가 살아 있는지를 나타내는 변수
+int task_log[MAX][2];               // 작업 로그 배열 (생산자, 소비자)
+_Atomic int produced = 0;            // 생산된 아이템 수
+_Atomic int consumed = 0;            // 소비된 아이템 수
 
-// 락 프리 스핀락 구현
-_Atomic int lock = 0;
+bool alive = true;                   // 쓰레드 실행 여부를 제어하기 위한 변수
+
+bool lock = false;                   // 스핀락 변수
 
 // 스핀락 획득 함수
-void spinlock_acquire(_Atomic int *lock) {
-    int expected = 0;
-    while (!atomic_compare_exchange_weak(lock, &expected, 1)) {
-        expected = 0;
-    }
+void acquire_lock(bool *lock) {
+    while (__atomic_test_and_set(lock, __ATOMIC_ACQUIRE));
 }
 
 // 스핀락 해제 함수
-void spinlock_release(_Atomic int *lock) {
-    *lock = 0;
+void release_lock(bool *lock) {
+    __atomic_clear(lock, __ATOMIC_RELEASE);
 }
 
 // 생산자 스레드 함수
 void *producer(void *arg) {
-    int i = *(int *)arg;
-    int item;
-    
+    int i = *(int *)arg;  // 스레드 ID
+    int item;             // 생산된 아이템
+
     while (alive) {
-        // 아이템을 생성할 때마다 next_item 값을 읽어와서 사용
-        item = atomic_fetch_add(&next_item, 1);
-        
-        // 스핀락 획득
-        spinlock_acquire(&lock);
-        
-        if (counter < BUFSIZE) {
-            buffer[in] = item;
-            in = (in + 1) % BUFSIZE;
-            counter++;
-            // task_log 배열의 인덱스 계산 수정
-            if (task_log[item % MAX][0] == -1) {
-                task_log[item % MAX][0] = i;
-                produced++;
+        acquire_lock(&lock);  // 락 획득
+
+        item = __atomic_fetch_add(&next_item, 1, __ATOMIC_RELAXED);  // 다음 아이템 번호 가져오기
+
+        if (counter < BUFSIZE) {  // 버퍼에 여유 공간이 있는 경우
+            buffer[in] = item;    // 버퍼에 아이템 추가
+            in = (in + 1) % BUFSIZE;  // 다음 생산자가 넣을 위치 갱신
+            __atomic_fetch_add(&counter, 1, __ATOMIC_RELAXED);  // 버퍼 안의 아이템 수 증가
+
+            // 작업 로그 갱신
+            if (task_log[item][0] == -1) {
+                task_log[item][0] = i;
+                __atomic_fetch_add(&produced, 1, __ATOMIC_RELAXED);
             } else {
                 printf("<P%d,%d>....ERROR: 아이템 %d 중복생산\n", i, item, item);
+                release_lock(&lock);  // 락 해제
+                continue;
             }
-            printf("<P%d,%d>\n", i, item);
-        } else {
-            // 버퍼가 가득 찬 경우, 스핀락 해제 후 잠시 대기
-            spinlock_release(&lock);
-            usleep(100);
-            continue;
+            printf("<P%d,%d>\n", i, item);  // 생산된 아이템 로그 출력
         }
-        /*
-         * 생산한 아이템을 출력한다.
-         */
-        printf("<P%d,%d>\n", i, item);
-        // 스핀락 해제
-        spinlock_release(&lock);
+
+        release_lock(&lock);  // 락 해제
     }
     pthread_exit(NULL);
 }
 
 // 소비자 스레드 함수
 void *consumer(void *arg) {
-    int i = *(int *)arg;
-    int item;
-    
+    int i = *(int *)arg;  // 스레드 ID
+    int item;             // 소비된 아이템
+
     while (alive) {
-        // 스핀락 획득
-        spinlock_acquire(&lock);
-        
-        if (counter > 0) {
-            item = buffer[out];
-            out = (out + 1) % BUFSIZE;
-            counter--;
-            if (task_log[item % MAX][0] == -1) {
+        acquire_lock(&lock);  // 락 획득
+
+        if (counter > 0) {  // 버퍼에 아이템이 있는 경우
+            item = buffer[out];    // 버퍼에서 아이템 가져오기
+            out = (out + 1) % BUFSIZE;  // 다음 소비자가 꺼낼 위치 갱신
+            __atomic_fetch_sub(&counter, 1, __ATOMIC_RELAXED);  // 버퍼 안의 아이템 수 감소
+
+            // 작업 로그 갱신
+            if (task_log[item][0] == -1) {
                 printf(RED"<C%d,%d>"RESET"....ERROR: 아이템 %d 미생산\n", i, item, item);
+                release_lock(&lock);  // 락 해제
+                continue;
+            } else if (task_log[item][1] == -1) {
+                task_log[item][1] = i;
+                __atomic_fetch_add(&consumed, 1, __ATOMIC_RELAXED);
             } else {
-                if (task_log[item % MAX][1] == -1) {
-                    task_log[item % MAX][1] = i;
-                    consumed++;
-                } else {
-                    printf(RED"<C%d,%d>"RESET"....ERROR: 아이템 %d 중복소비\n", i, item, item);
-                    continue;
-                }
+                printf(RED"<C%d,%d>"RESET"....ERROR: 아이템 %d 중복소비\n", i, item, item);
+                release_lock(&lock);  // 락 해제
+                continue;
             }
-            printf(RED"<C%d,%d>"RESET"\n", i, item);
+            printf(RED"<C%d,%d>"RESET"\n", i, item);  // 소비된 아이템 로그 출력
         }
-        
-        // 스핀락 해제
-        spinlock_release(&lock);
+
+        release_lock(&lock);  // 락 해제
     }
     pthread_exit(NULL);
 }
 
 int main(void) {
-    pthread_t tid[N];
+    pthread_t tid[N];  // 스레드 ID 배열
     int i, id[N];
 
-    // task_log 배열 초기화
+    // 작업 로그 배열 초기화
     for (i = 0; i < MAX; ++i)
         task_log[i][0] = task_log[i][1] = -1;
 
@@ -136,22 +127,23 @@ int main(void) {
         pthread_create(tid+i, NULL, producer, id+i);
     }
 
-    usleep(RUNTIME);
-    alive = false;
+    usleep(RUNTIME);  // 실행 시간 제한
 
-    // 스레드 종료 대기
+    alive = false;  // 쓰레드 실행 종료 요청
+
+    // 모든 스레드 종료 대기
     for (i = 0; i < N; ++i)
         pthread_join(tid[i], NULL);
 
-    // 미소비 확인
-    for (i = 0; i < consumed; ++i)
-        if (task_log[i][1] == -1) {
+    // 모든 아이템이 소비되었는지 확인
+    for (i = 0; i < MAX; ++i)
+        if (task_log[i][1] == -1 && task_log[i][0] != -1) {
             printf("....ERROR: 아이템 %d 미소비\n", i);
             return 1;
         }
 
-    // 생산된 아이템과 소비된 아이템의 수 출력
-    if (next_item == produced) {
+    // 생산된 아이템 수와 소비된 아이템 수 일치 여부 확인
+    if (produced == consumed) {
         printf("Total %d items were produced.\n", produced);
         printf("Total %d items were consumed.\n", consumed);
     } else {
